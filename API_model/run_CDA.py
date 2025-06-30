f18ab7539bc5c47792ba6ec85a421c7b45286c43
Samuel Hollowood <141444154+Samhollowood@users.noreply.github.com>
2025-06-30 11:18:30 +0100

    Update run_CDA.py

--- API_model/run_CDA.py
+++ API_model/run_CDA.py
@@ -28,9 +28,8 @@ import cavecalc.analyse as cca
 s =  {#Environmental model inputs
       'soil_d13C': [-18,-21,-25],
       'soil_pCO2': [260, 600, 1000, 2000, 5000, 8000],
-      'gas_volume': [0, 100, 200, 300, 400, 500],
-      'cave_pCO2': [260, 600, 4500],
-      'temperature': [5,15,25],
+      'gas_volume': [0,50, 200, 500],
+      'cave_pCO2': [260],
       'atm_d18O': [-10,-15,-20],
       'bedrock_MgCa': [50,100,150],
 
